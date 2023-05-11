c164c27223c7f9e4aca881ec5ed48871025efe1e
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-11 04:29:09 +0400

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+
+            String str = new String(data, "UTF-8");
+            String[] lines = str.split("\n");
+
+            for (String line : lines) {
+                String[] parts = line.split(": ");
+                parts[1] = parts[1].trim();
+                switch (parts[0]) {
+                    case "Name":
+                        profile.setName(parts[1]);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(parts[1]));
+                        break;
+                    case "Email":
+                        profile.setEmail(parts[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(parts[1]));
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
